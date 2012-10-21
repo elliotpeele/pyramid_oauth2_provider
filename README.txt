04b86fa3c754a0cfccb5e08e029ce2b08786ebbd
Elliot Peele <elliot@bentlogic.net>
2012-10-20 23:22:03 -0400

    initial project creation

---
+++ README.txt
@@ -0,0 +1,14 @@
+pyramid_oauth2_provider README
+==================
+
+Getting Started
+---------------
+
+- cd <directory containing this file>
+
+- $venv/bin/python setup.py develop
+
+- $venv/bin/populate_pyramid_oauth2_provider development.ini
+
+- $venv/bin/pserve development.ini
+
---
+++ setup.py
@@ -0,0 +1,45 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'pyramid',
+    'SQLAlchemy',
+    'transaction',
+    'pyramid_tm',
+    'pyramid_debugtoolbar',
+    'zope.sqlalchemy',
+    'waitress',
+    ]
+
+setup(name='pyramid_oauth2_provider',
+      version='0.0',
+      description='pyramid_oauth2_provider',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='pyramid_oauth2_provider',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = pyramid_oauth2_provider:main
+      [console_scripts]
+      initialize_pyramid_oauth2_provider_db = pyramid_oauth2_provider.scripts.initializedb:main
+      """,
+      )
+
