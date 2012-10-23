69242fdb3fcee61ea3c7c69cc9eb71ac79a30391
Elliot Peele <elliot@bentlogic.net>
2012-10-23 00:32:05 -0400

    update license headers

--- pyramid_oauth2_provider/__init__.py
+++ pyramid_oauth2_provider/__init__.py
@@ -1,3 +1,15 @@
+#
+# Copyright (c) Elliot Peele <elliot@bentlogic.net>
+#
+# This program is distributed under the terms of the MIT License as found·
+# in a file called LICENSE. If it is not present, the license
+# is always available at http://www.opensource.org/licenses/mit-license.php.
+#
+# This program is distributed in the hope that it will be useful, but
+# without any waranty; without even the implied warranty of merchantability
+# or fitness for a particular purpose. See the MIT License for full details.
+#
+
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
 
@@ -13,4 +25,3 @@ def main(global_config, **settings):
     config.add_route('home', '/')
     config.scan()
     return config.make_wsgi_app()
-
