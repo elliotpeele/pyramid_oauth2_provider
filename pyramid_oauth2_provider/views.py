322603597216b14d001b72efe382783e3882c1a7
Elliot Peele <elliot@bentlogic.net>
2012-10-23 01:09:46 -0400

    initial view for token endpoint

--- pyramid_oauth2_provider/views.py
+++ pyramid_oauth2_provider/views.py
@@ -1,34 +1,66 @@
+#
+# Copyright (c) Elliot Peele <elliot@bentlogic.net>
+#
+# This program is distributed under the terms of the MIT License as found·
+# in a file called LICENSE. If it is not present, the license
+# is always available at http://www.opensource.org/licenses/mit-license.php.
+#
+# This program is distributed in the hope that it will be useful, but
+# without any waranty; without even the implied warranty of merchantability
+# or fitness for a particular purpose. See the MIT License for full details.
+#
+
 from pyramid.response import Response
 from pyramid.view import view_config
 
 from sqlalchemy.exc import DBAPIError
 
-from .models import (
-    DBSession,
-    MyModel,
-    )
+from .models import DBSession
+from .models import MyModel
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    try:
-        one = DBSession.query(MyModel).filter(MyModel.name=='one').first()
-    except DBAPIError:
-        return Response(conn_err_msg, content_type='text/plain', status_int=500)
-    return {'one':one, 'project':'pyramid_oauth2_provider'}
+@view_config(route_name='oauth2_token', renderer='json')
+def oauth2_token(request):
+    """
+    * In the case of an incoming authentication request a POST is made
+    with the following structure.
 
-conn_err_msg = """\
-Pyramid is having a problem using your SQL database.  The problem
-might be caused by one of the following things:
+        POST /token HTTP/1.1
+        Host: server.example.com
+        Authorization: Basic czZCaGRSa3F0MzpnWDFmQmF0M2JW
+        Content-Type: application/x-www-form-urlencoded
 
-1.  You may need to run the "initialize_pyramid_oauth2_provider_db" script
-    to initialize your database tables.  Check your virtual 
-    environment's "bin" directory for this script and try to run it.
+        grant_type=password&username=johndoe&password=A3ddj3w
 
-2.  Your database server may not be running.  Check that the
-    database server referred to by the "sqlalchemy.url" setting in
-    your "development.ini" file is running.
+    The basic auth header contains the client_id:client_secret base64
+    encoded for client authentication.
 
-After you fix the problem, please restart the Pyramid application to
-try it again.
-"""
+    The username and password are form encoded as part of the body. This
+    request *must* be made over https.
 
+    The response to this request will be, assuming no error:
+
+        HTTP/1.1 200 OK
+        Content-Type: application/json;charset=UTF-8
+        Cache-Control: no-store
+        Pragma: no-cache
+
+        {
+          "access_token":"2YotnFZFEjr1zCsicMWpAA",
+          "token_type":"bearer",
+          "expires_in":3600,
+          "refresh_token":"tGzv3JOkF0XG5Qx2TlKWIA",
+        }
+
+    * In the case of a token refresh request a POST with the following
+    structure is required:
+
+        POST /token HTTP/1.1
+        Host: server.example.com
+        Authorization: Basic czZCaGRSa3F0MzpnWDFmQmF0M2JW
+        Content-Type: application/x-www-form-urlencoded
+
+        grant_type=refresh_token&refresh_token=tGzv3JOkF0XG5Qx2TlKWIA
+
+    The response will be the same as above with a new access_token and
+    refresh_token.
+    """
