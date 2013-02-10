58a93f2133f2c7850ed469f64dbad624ff0fcd09
Elliot Peele <elliot@bentlogic.net>
2013-02-09 19:59:51 -0500

    fix typo in license header, thanks to ramsay for pointing this out

--- pyramid_oauth2_provider/scripts/__init__.py
+++ pyramid_oauth2_provider/scripts/__init__.py
@@ -6,6 +6,6 @@
 # is always available at http://www.opensource.org/licenses/mit-license.php.
 #
 # This program is distributed in the hope that it will be useful, but
-# without any waranty; without even the implied warranty of merchantability
+# without any warrenty; without even the implied warranty of merchantability
 # or fitness for a particular purpose. See the MIT License for full details.
 #
